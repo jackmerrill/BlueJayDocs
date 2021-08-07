a3bf486fb65035c25fadfc9e0e4f4b618a6f3f45
Matt Ronchetto <hello@doamatto.xyz>
2021-08-06 21:24:18 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,14 @@
 const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
 module.exports = withNextra()
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/security',
+        destination: '/legal/security',
+        permanent: false
+      }
+    ]
+  },
+};
+  
